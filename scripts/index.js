cffaad08071f5b5c72f0436eb1a7eb0d3aeb0856
Albert Narimanov <albertnarimanov@gmail.com>
2024-06-21 10:58:50 +0300

    Finished project

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const cardList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
+function addCard(src, title) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
-// @todo: Функция удаления карточки
+  cardElement.querySelector(".card__image").src = src;
+  cardElement.querySelector(".card__title").textContent = title;
+  cardElement.querySelector(".card__image").alt = 'Картинка города ' + title;
+  deleteButton.addEventListener("click", removeCard);
+
+  cardList.append(cardElement);
+}  
+  // @todo: Функция удаления карточки
+function removeCard(event) {
+    event.target.closest('.card').remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((card) => addCard(card.link, card.name));
